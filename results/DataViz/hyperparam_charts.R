3b6acb339b5f2e35e461bc9be5ea08411114bafa
Adam Goodkind <a.goodkind@gmail.com>
2018-03-24 19:06:16 -0400

    updating visualization script

--- results/DataViz/hyperparam_charts.R
+++ results/DataViz/hyperparam_charts.R
@@ -6,26 +6,38 @@ param.summ$nhid <- as.factor(param.summ$nhid)
 param.summ$nlayers <- as.factor(param.summ$nlayers)
 summary(param.summ)
 
-
-
 ggplot(param.summ) +
   geom_bar(aes(epoch, val_acc, fill="val_acc"), stat='summary', fun.y='mean') +
   geom_bar(aes(epoch, val_prec, fill="val_prec"), stat='summary', fun.y='mean')
 
 ## accuracy  
+labels_layers <- c('1'="1 Layer", '2'="2 Layers")
+labels_hid <- c('32'="32 Units", '64'="64 Units")
+
 ggplot(param.summ, aes(x=epoch, y=val_acc, color=emsize, group=interaction(condition,run))) +
   geom_line() +
-  facet_grid(nlayers ~ nhid)
+  facet_grid(nlayers ~ nhid, labeller=labeller(nlayers=labels_layers,
+                                               nhid=labels_hid)) +
+  labs(title='Accuracy on validation set by epochs
+       By EM Size, Hidden Layers and Unit Count',
+       x='Number of epochs',
+       y='Accuracy on validation set') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  # ggsave('epoch-val_acc_facets.pdf', device='pdf')
+  ggsave('epoch-val_acc_facets.jpeg', device='jpeg')
   
-  # geom_point(aes(y=val_acc, color=emsize, group=interaction(condition,run))) +
-  stat_summary(fun.y='median', geom='line') +
-  geom_smooth(method='loess', se=FALSE)
-
 ggplot(param.summ, aes(x=epoch)) +
   stat_summary(aes(y=val_acc, color=nhid), fun.y='mean', geom='line')
 
 ggplot(param.summ, aes(x=epoch)) +
-  stat_summary(aes(y=val_acc, color=nlayers), fun.y='mean', geom='line')
+  stat_summary(aes(y=val_acc, color=nlayers), fun.y='mean', geom='line')+
+  labs(title='Accuracy on validation set by epochs
+       Ignoring Speaker Info',
+       x='Number of epochs',
+       y='Accuracy on validation set') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  # ggsave('epoch-val_acc_nlayers.pdf', device='pdf')
+  ggsave('epoch-val_acc_nlayers.jpeg', device='jpeg')
 
 ## loss
 ggplot(param.summ, aes(x=epoch)) +
@@ -76,7 +88,40 @@ ggplot(param.summ) +
 ggplot(param.summ) +
   geom_bar(aes(nlayers, epoch_time), stat='summary', fun.y='mean')
 
+################### ALSO INCLUDE SPEAKER ############################
 
-  # stat_summary(aes(y=val_prec), fun.y='mean', geom='line') +
-  # stat_summary(aes(y=val_recall), fun.y='mean', geom='line') +
-  # labs(y='')
+param.summ.speaker <- read_csv('summary_all_speaker.csv')
+param.summ.speaker$emsize <- as.factor(param.summ.speaker$emsize)
+param.summ.speaker$nhid <- as.factor(param.summ.speaker$nhid)
+param.summ.speaker$nlayers <- as.factor(param.summ.speaker$nlayers)
+summary(param.summ.speaker)
+
+## accuracy  
+ggplot(param.summ.speaker, aes(x=epoch)) +
+   stat_summary(aes(y=val_acc, color=ignore_speaker), fun.y='mean', geom='line') +
+  labs(title='Accuracy on validation set by epochs',
+       x='Number of epochs',
+       y='Accuracy on validation set (%)') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  # ggsave('epoch-val_acc_all.pdf', device='pdf')
+  ggsave('epoch-val_acc_all.jpeg', device='jpeg')
+
+## loss
+ggplot(param.summ.speaker, aes(x=epoch)) +
+  stat_summary(aes(y=val_loss, color=ignore_speaker), fun.y='mean', geom='line') +
+  labs(title='Loss on validation set by epochs',
+     x='Number of epochs',
+     y='Loss on validation set') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  # ggsave('epoch-val_loss_all.pdf', device='pdf')
+  ggsave('epoch-val_loss_all.jpeg', device='jpeg')
+
+## f-score
+ggplot(param.summ.speaker, aes(x=epoch)) +
+  stat_summary(aes(y=val_f, color=ignore_speaker), fun.y='mean', geom='line') +
+  labs(title='F-score on validation set by epochs',
+       x='Number of epochs',
+       y='F-score on validation set') +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  # ggsave('epoch-val_f_all.pdf', device='pdf')
+  ggsave('epoch-val_f_all.jpeg', device='jpeg')
